31de9cbe9d1434c9ba4928e83bafc1689b584e41
Tom Haber <46713612+tomhaberdin@users.noreply.github.com>
2025-02-20 11:54:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,25 +2,43 @@
 import streamlit as st
 from langchain.llms import openai
 from langchain.llms import OpenAI
+from langchain_openai import ChatOpenAI
+from openai import OpenAI
+import os 
 
-st.title('my first gen AI app_tom')
+st.title('My First Gen AI')
 
 st.markdown("""
-this is a sample markdown
-try it on your 'own'
+Welcome to the Pirate App!
 """)
 
 openai_api_key = st.sidebar.text_input("open AI key")
-name = st.text_input("enter some text", "enter here")
-option = st.radio("choose one option:", options = ["Option1", "Option2"], index=0)
 
-value = st.slider("Enter a value: ", 0, 100, 20)
-print(value)
-print(option)
+# possible elements to set in the app
+#name = st.text_input("enter some text", "enter here")
+#option = st.radio("choose one option:", options = ["Option1", "Option2"], index=0)
+
+
+# setting tempature
+temperature_value = st.slider("Enter a value: ", 0.0, 2.0, 0.3)
+
+print(temperature_value)
+print(openai_api_key)
+
+os.environ["OPENAI_API_KEY"] = openai_api_key
+
 
 def gen_response(txt):
-    llm = OpenAI(temperature = 0.7, openai_api_key=openai_api_key)
-    st.info(llm(txt))
+    #llm = OpenAI(temperature_value = 0.7, openai_api_key=openai_api_key)
+    # starting llm model
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    # setting system and human question
+    messages = [
+                ("system","you are a very friendly pirate and always ansewr in a cool pirat song, and the reply must be in hebrew"), # machine part role
+                ("human",txt), # the human asking the question
+                ]
+    ans = llm.invoke(messages,config={"temperature":temperature_value})
+    st.info(ans.content)
 
 with st.form("sample app"):
     txt = st.text_area("enter text:", "what GPT stands for")
@@ -28,3 +46,5 @@ with st.form("sample app"):
     if subm:
         gen_response(txt)
         
+
+
